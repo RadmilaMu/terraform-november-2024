0b44c3af1587ba23a067623373b4d2958adf91ce
RadmilaMu <rada171997@gmail.com>
2025-02-01 04:51:48 +0000

    class6

--- Class6-module/main.tf
+++ Class6-module/main.tf
@@ -1,7 +1,7 @@
-module "november-2024" {
-  source  = "RadmilaMu/november-2024/module"
-  version = "1.0.0"
+module "novembe-2024r" {
+  source  = "RadmilaMu/novembe-2024r/module"
+  version = "2.0.0"
   region = "us-east-1"
   vpc_cidr = "10.0.0.0/16"
-  subnet_cidr = "10.0.1.0/16"
+  subnet_cidr = "10.0.1.0/24"
 }
